d6f4cedd4d019a256b5a778d620389b5e8d69b6c
Oscar Pérez <99992053+osipzmj@users.noreply.github.com>
2023-03-09 19:39:56 -0600

    Add files via upload

---
+++ Mini REED/Mini reed.ino
@@ -0,0 +1,20 @@
+// for ESP32 microcontroller
+/int analogPin = 4; 
+int digitalPin = 2;
+
+void setup() {
+  pinMode(analogPin, INPUT); 
+  pinMode(digitalPin, INPUT); 
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogVal = analogRead(analogPin);
+  int digitalVal = digitalRead(digitalPin);
+  
+  Serial.print(analogVal);
+  Serial.print(" - ");
+  Serial.println(digitalVal);
+  
+  delay(100);
+}
